f1e1371d96e7a7171b4a3976c2d3a0f6d53c4716
hudiilfeld <hudifeld@gmail.com>
2019-01-04 02:18:19 +0200

    minor/unsignicant changes

--- app/src/main/java/com/hudiilfeld/shiurdiario/views/HomeActivity.java
+++ app/src/main/java/com/hudiilfeld/shiurdiario/views/HomeActivity.java
@@ -112,6 +112,7 @@ public class HomeActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.home) {
+
             Intent intent = getIntent();
             overridePendingTransition(0, 0);
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
@@ -120,11 +121,13 @@ public class HomeActivity extends AppCompatActivity
             startActivity(intent);
 
         } else if (id == R.id.daf_hayomi) {
+
             Intent intent = new Intent(this, DafHayomiActivity.class);
             intent.putExtra(PREFIX, prefix);
             intent.putExtra(MASECHET, masechet);
             intent.putExtra(DAF, daf);
             startActivity(intent);
+
         } else if (id == R.id.shiurim) {
 
             getSupportFragmentManager()
